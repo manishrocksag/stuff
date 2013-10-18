422969439e133eec86578325265820720f19631a
Manish Agarwal <manishrocksag@gmail.com>
2013-10-18 17:22:46 +0530

    No of Triangles that can be formed

---
+++ Arrays/NoOfTriangles.java
@@ -0,0 +1,23 @@
+public class NoOfTriangles {
+    public static int noOfTriangles(int[] arr){
+        int count=0;
+        int n=arr.length;
+        for(int i=0;i<n-2;++i){
+            int k=i+2;
+            for(int j=i+1;j<n;++j){
+                while(k<n && arr[i]+arr[j]>arr[k]){
+                    ++k;
+                }
+                count+=k-j-1;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+    int arr[] =   {10, 21, 22, 100, 101, 200, 300};
+        System.out.println(NoOfTriangles.noOfTriangles(arr));
+}
+        
+    
+    
+}
